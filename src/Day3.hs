2edc8e0ac05aeac8d04d5077526a95c95e24cb7b
Markus Klinik <mkl@lambdanaut.net>
2023-12-05 09:06:32 +0100

    day 3

---
+++ src/Day3.hs
@@ -0,0 +1,77 @@
+module Day3 where
+
+import Text.Megaparsec
+import Data.Char
+
+import Util
+
+day3 :: String -> Int
+day3 input = sum partNumbers
+  where
+  allTokens = myParse day3P input
+  symbols = [s | S s <- allTokens]
+  numbers = [n | N n <- allTokens]
+  partNumbers =
+    [ value
+    | n@(Number _ value _) <- numbers
+    , s <- symbols
+    , isAdjacent s n
+    ]
+
+day3_2 :: String -> Int
+day3_2 input = sum [n1 * n2 | (n1, n2) <- gears]
+  where
+  allTokens = myParse day3P input
+  stars = [s | S s@(Symbol _ '*') <- allTokens]
+  numbers = [n | N n <- allTokens]
+  gears =
+    [ (n1, n2)
+    | s <- stars
+    , [Number _ n1 _, Number _ n2 _] <- [filter (isAdjacent s) numbers]
+    ]
+
+type MySourcePos = (Int, Int)
+
+mkMyPos :: SourcePos -> MySourcePos
+mkMyPos p = (line, col)
+  where
+  line = unPos $ sourceLine p
+  col = unPos $ sourceColumn p
+
+myPosP :: Parser MySourcePos
+myPosP = mkMyPos <$> getSourcePos
+
+data Number = Number MySourcePos Int MySourcePos
+  deriving (Show, Eq)
+
+data Symbol = Symbol MySourcePos Char
+  deriving (Show, Eq)
+
+data Tok
+  = N Number
+  | Dot
+  | S Symbol
+  | Ws
+  deriving (Show, Eq)
+
+numberP :: Parser Number
+numberP = Number <$> myPosP <*> numP <*> myPosP
+
+symbolP :: Parser Symbol
+symbolP = Symbol <$> myPosP <*> satisfy (not . isSpace)
+
+tokenP :: Parser Tok
+tokenP =
+      Dot <$ single '.'
+  <|> N <$> numberP
+  <|> S <$> symbolP
+  <|> Ws <$ ws
+
+day3P :: Parser [Tok]
+day3P = many tokenP
+
+isAdjacent :: Symbol -> Number -> Bool
+isAdjacent (Symbol (sy, sx) _) (Number (ny, nx1) _ (_, nx2)) = yAdjacent && xAdjacent
+  where
+  yAdjacent = abs (sy - ny) <= 1
+  xAdjacent = sx >= nx1 - 1 && sx <= nx2
---
+++ test/Day3Spec.hs
@@ -0,0 +1,48 @@
+module Day3Spec (spec) where
+
+import Test.Hspec
+import Control.Monad.IO.Class
+import Text.Megaparsec
+import Data.List
+
+import Day3
+import Util
+
+spec :: Spec
+spec = fdescribe "Day3" $ do
+  it "parses tokens" $ do
+    myParse tokenP "123" `shouldBe` N (Number (1,1) 123 (1,4))
+    myParse tokenP "." `shouldBe` Dot
+    myParse tokenP "*" `shouldBe` S (Symbol (1,1) '*')
+
+  it "parses the example" $ do
+    input <- liftIO $ readFile "day3example.txt"
+    myParse day3P input `shouldSatisfy` \r ->
+      [ N $ Number (1,1) 467 (1,4)
+      , Dot, Dot
+      , N $ Number (1,6) 114 (1,9)
+      , Dot, Dot, Ws
+      , Dot, Dot, Dot
+      , S $ Symbol (2, 4) '*'
+      ]
+      `isPrefixOf` r
+
+  it "calculates adjacency" $ do
+    isAdjacent (Symbol (2, 4) '*') (Number (1,1) 467 (1,4)) `shouldBe` True
+    isAdjacent (Symbol (2, 4) '*') (Number (1,6) 114 (1,9)) `shouldBe` False
+
+  it "solves the example" $ do
+    input <- liftIO $ readFile "day3example.txt"
+    day3 input `shouldBe` 4361
+
+  it "solves the puzzle" $ do
+    input <- liftIO $ readFile "day3.txt"
+    day3 input `shouldBe` 498559
+
+  it "solves the example part 2" $ do
+    input <- liftIO $ readFile "day3example.txt"
+    day3_2 input `shouldBe` 467835
+
+  it "solves part 2" $ do
+    input <- liftIO $ readFile "day3.txt"
+    day3_2 input `shouldBe` 72246648
